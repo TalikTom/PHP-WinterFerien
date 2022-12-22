dec82c6dc119b49f161f2d6c0c84145cd2eb4d54
Luka Agic <lukagic@gmail.com>
2022-12-22 23:38:54 +0100

    Create pages and add links

--- tailwind/calendar.view.php
+++ tailwind/calendar.view.php
@@ -211,14 +211,14 @@
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-                    Home page
+                    Calendar page
                 </h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Replace with your content -->
-                <p>This is the home page</p>
+                <p>This is a page for a calendar</p>
                 <!-- /End replace -->
             </div>
         </main>
