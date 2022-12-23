3e045a0a73b1424c6922704b1d9381f44e9ca97a
Luka Agic <lukagic@gmail.com>
2022-12-23 23:16:35 +0100

    Small fix

--- bootstrap/views/partials/header.php
+++ bootstrap/views/partials/header.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="../../css/style.css" rel="stylesheet" type="text/css" />
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../bootstrap/css/style.css" rel="stylesheet" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
   </head>
