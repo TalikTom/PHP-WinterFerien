8cbffa78b88c33c00987818c13f207b2e38de35e
Luka Agic <lukagic@gmail.com>
2022-12-24 23:29:01 +0100

    Update bootstrap, create pages

--- bootstrap/views/calendar.view.php
+++ bootstrap/views/calendar.view.php
@@ -2,8 +2,8 @@
 <main>
 <body>
 
-
 <?php require 'partials/nav.php'; ?>
+
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
           <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
         </form>
