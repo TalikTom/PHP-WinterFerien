e9e790dbcd255132f666ac407240eedf032452cf
Luka Agic <lukagic@gmail.com>
2022-12-27 00:15:36 +0100

    Add nav and boxes to "customcss"

---
+++ customcss/views/includes/nav.php
@@ -0,0 +1,9 @@
+<nav class="main-nav">
+    <ul>
+        <li><a href="#"></a>Home</li>
+        <li><a href="#"></a>About us</li>
+        <li><a href="#"></a>Calendar</li>
+        <li><a href="#"></a>Contact us</li>
+    </ul>
+</nav>
+
--- customcss/views/index.view.php
+++ customcss/views/index.view.php
@@ -1,4 +1,26 @@
 <?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
 <?php include 'includes/banner.php'; ?>
-
+<section class="boxes">
+      <div class="box">
+        <i class="fas fa-chart-pie fa-4x"></i>
+        <h3>Analytics</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+      <div class="box">
+        <i class="fas fa-globe fa-4x"></i>
+        <h3>Marketing</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+      <div class="box">
+        <i class="fas fa-cog fa-4x"></i>
+        <h3>Development</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+      <div class="box">
+        <i class="fas fa-users fa-4x"></i>
+        <h3>Support</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+    </section>
 <?php include 'includes/footer.php'; ?>
