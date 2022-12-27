f1453d662c53560639b32258fbe67004305b8263
Luka Agic <lukagic@gmail.com>
2022-12-27 19:06:08 +0100

    Add router in bootstrap files

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,3 +1,21 @@
 <?php
+
+
 require 'functions.php';
-require 'views/index.view.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes =
+    [
+        '/bootstrap/' => 'controllers/index.php',
+        '/about' => 'controllers/about.php',
+        '/calendar' => 'controllers/calendar.php',
+        '/contact' => 'controllers/contact.php',
+    ];
+
+if (array_key_exists($uri, $routes))
+{
+    require $routes[$uri];
+}
+
+?>
