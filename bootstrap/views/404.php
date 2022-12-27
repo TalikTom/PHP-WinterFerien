e3cb071b41f647a6686fbadc67a714eb3d710b4f
Luka Agic <lukagic@gmail.com>
2022-12-27 23:12:30 +0100

    Add 404 page in bootstrap

---
+++ bootstrap/views/404.php
@@ -0,0 +1,13 @@
+<?php require 'partials/header.php'; ?>
+<?php require 'partials/nav.php'; ?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
+            <h1 class="text-2xl font-bold">Sorry, page not found</h1>
+            <p class="mt-4">
+                <a href="/bootstrap/" class="text-primary underline">Go back home</a>
+            </p>
+        </div>
+    </div>
+</div>
+<?php require 'partials/footer.php'; ?>
