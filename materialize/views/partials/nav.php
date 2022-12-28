1d84cc7682603fc61887bc5a86303d6875596484
Luka Agic <lukagic@gmail.com>
2022-12-28 21:57:35 +0100

    Initial commit for materialize files

---
+++ materialize/views/partials/nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo right">Logo</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="/materialize/">Sass</a></li>
+            <li><a href="materialize/about">About us</a></li>
+            <li><a href="materialize/calendar">Calendar</a></li>
+            <li><a href="materialize/contact">Contact us</a></li>
+        </ul>
+    </div>
+</nav>
