fd43c2972c00c19e8fb9810d47651134916342b5
Luka Agic <lukagic@gmail.com>
2022-12-30 22:19:28 +0100

    Add css grid_content

---
+++ customcss/views/partials/cards.php
@@ -0,0 +1,21 @@
+<div class="cards-wrapper">
+    <h2>Lorem, ipsum.</h2>
+    <div class="cards">
+        <div class="card">
+            <div class="content-img"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <div class="card">
+            <div class="content-img"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <div class="card">
+            <div class="content-img"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.illustration or image</p>
+        </div>
+        <div class="card">
+            <div class="content-img"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</div>
---
+++ customcss/views/partials/grid_content.php
@@ -0,0 +1,14 @@
+<div class="grid-wrapper">
+    <div class="card">Lorem ipsum dolor sit amet.
+        <p></p>
+    </div>
+    <div class="card">Lorem ipsum dolor sit amet.
+        <p></p>
+    </div>
+    <div class="card">Lorem ipsum dolor sit amet.
+        <p></p>
+    </div>
+    <div class="card">Lorem ipsum dolor sit amet.
+        <p></p>
+    </div>
+</div>
