e54f2ec24813daff3f36affdd5410bbe5ae97d09
Luka Agic <lukagic@gmail.com>
2022-12-30 20:50:26 +0100

    Add responsive images in banner

--- customcss/views/partials/banner.php
+++ customcss/views/partials/banner.php
@@ -1,9 +1,15 @@
 <!-- Swiper -->
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
-        <div class="swiper-slide"><img src="/customcss/img/image1.jpg" alt=""></div>
-        <div class="swiper-slide"><img src="/customcss/img/image2.jpg" alt=""></div>
-        <div class="swiper-slide"><img src="/customcss/img/image3.jpg" alt=""></div>
+        <div class="swiper-slide">
+            <picture>
+                <source media="(max-width:650px)" srcset="/customcss/img/image1-650.jpg">
+                <source media="(max-width:450px)" srcset="/customcss/img/image1-450.jpg">
+                <img src="/customcss/img/image1.jpg" alt="Elder care">
+            </picture>
+        </div>
+        <div class="swiper-slide"><img src="/customcss/img/image2.jpg" alt="Elder care"></div>
+        <div class="swiper-slide"><img src="/customcss/img/image3.jpg" alt="Elder care"></div>
     </div>
     <div class="swiper-pagination"></div>
 </div>
