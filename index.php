4f904a46caf2027bd9605403176b0934412b0558
Luka Agic <lukagic@gmail.com>
2022-12-31 22:20:30 +0100

    Style home page

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 
 
 <nav class="wrapper">
-    <h1>Zadatak</h1>
-    <ul class="nav-container">
-        <li><a href="/customcss">customcss</a></li>
-        <li><a href="/bootstrap">bootstrap</a></li>
-        <li><a href="/foundation">foundation</a></li>
-        <li><a href="/tailwind">tailwind</a></li>
-        <li><a href="/materialize">materialize</a></li>
-    </ul>
+    <div class="content-wrapper">
+        <h1>Zadatak</h1>
+        <ul class="nav-container">
+            <li><a href="/customcss">customcss</a></li>
+            <li><a href="/bootstrap">bootstrap</a></li>
+            <li><a href="/foundation">foundation</a></li>
+            <li><a href="/tailwind">tailwind</a></li>
+            <li><a href="/materialize">materialize</a></li>
+        </ul>
+    </div>
+    </div>
 
 </nav>
