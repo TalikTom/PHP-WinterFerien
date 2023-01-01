0c7428e05c7c4e76194cd1d56fa1e0728c204895
Luka Agic <lukagic@gmail.com>
2023-01-01 23:51:21 +0100

    Style nav

--- foundation/index.php
+++ foundation/index.php
@@ -1,17 +1,16 @@
 <?php
-
+include 'functions.php';
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes =
     [
-        '/foundation/' => 'controllers/index.php',
-        '/foundation/about' => 'controllers/about.php',
-        '/foundation/calendar' => 'controllers/calendar.php',
-        '/foundation/contact' => 'controllers/contact.php',
-    ];
+    '/foundation/' => 'controllers/index.php',
+    '/foundation/about' => 'controllers/about.php',
+    '/foundation/calendar' => 'controllers/calendar.php',
+    '/foundation/contact' => 'controllers/contact.php',
+];
 
-if (array_key_exists($uri, $routes)) 
-{
+if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
     http_response_code(404);
@@ -20,5 +19,3 @@ if (array_key_exists($uri, $routes))
 
     die();
 }
-
-?>
--- foundation/views/partials/nav.php
+++ foundation/views/partials/nav.php
@@ -6,24 +6,26 @@
 <div class="top-bar" id="example-menu">
     <div class="top-bar-left">
         <ul class="dropdown menu" data-dropdown-menu>
-            <li class="menu-text">Site Title</li>
-            <li class="has-submenu">
-                <a href="/foundation/">Home</a>
-                <ul class="submenu menu vertical" data-submenu>
-                    <li><a href="#0">One</a></li>
-                    <li><a href="#0">Two</a></li>
-                    <li><a href="#0">Three</a></li>
-                </ul>
-            </li>
-            <li><a href="/foundation/about">About us</a></li>
-            <li><a href="/foundation/calendar">Calendar</a></li>
-            <li><a href="/foundation/contact">Contact us</a></li>
+            <li><a href="/foundation/" style="color: <?=urlIs('/foundation/') ? 'lightblue' : ''?>;">Home</a></li>
+            <li><a href="/foundation/about" style="color: <?=urlIs('/foundation/about') ? 'lightblue' : ''?>;">About
+                    us</a></li>
+            <li><a href="/foundation/calendar"
+                    style="color: <?=urlIs('/foundation/calendar') ? 'lightblue' : ''?>;">Calendar</a></li>
+            <li><a href="/foundation/contact"
+                    style="color: <?=urlIs('/foundation/contact') ? 'lightblue' : ''?>;">Contact
+                    us</a></li>
         </ul>
     </div>
     <div class="top-bar-right">
-        <ul class="menu">
-            <li><input type="search" placeholder="Search"></li>
-            <li><button type="button" class="button">Search</button></li>
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="has-submenu">
+                <a href="/foundation/">Profile</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="#0">My Profile</a></li>
+                    <li><a href="#0">Settings</a></li>
+                    <li><a href="#0">Log-out</a></li>
+                </ul>
+            </li>
         </ul>
     </div>
 </div>
