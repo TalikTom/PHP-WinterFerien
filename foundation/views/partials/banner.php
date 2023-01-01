d1cf967967b760612b40295da66c0a0cb0b4fbdb
Luka Agic <lukagic@gmail.com>
2023-01-01 23:27:51 +0100

    Fix foundation bullet banner menu

--- foundation/views/partials/banner.php
+++ foundation/views/partials/banner.php
@@ -1,28 +1,26 @@
 <div class="row">
-  <div class="columns">
-    <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-      <ul class="orbit-container">
-        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-        <li class="is-active orbit-slide">
-          <img class="orbit-image" src="/foundation/img/image1.jpg" alt="Space">
-          <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-        </li>
-        <li class="orbit-slide">
-          <img class="orbit-image" src="/foundation/img/image2.jpg" alt="Space">
-          <figcaption class="orbit-caption">Encapsulating</figcaption>
-        </li>
-        <li class="orbit-slide">
-          <img class="orbit-image" src="/foundation/img/image3.jpg" alt="Space">
-          <figcaption class="orbit-caption">Outta This World</figcaption>
-        </li>
-      </ul>
-      <nav class="orbit-bullets">
-        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-      </nav>
+    <div class="columns">
+        <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+            <ul class="orbit-container">
+                <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+                <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+                <li class="is-active orbit-slide">
+                    <img class="orbit-image" src="/foundation/img/image1.jpg" alt="Space">
+                </li>
+                <li class="orbit-slide">
+                    <img class="orbit-image" src="/foundation/img/image2.jpg" alt="Space">
+                </li>
+                <li class="orbit-slide">
+                    <img class="orbit-image" src="/foundation/img/image3.jpg" alt="Space">
+                </li>
+                <nav class="orbit-bullets">
+                    <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span
+                            class="show-for-sr">Current Slide</span></button>
+                    <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+                    <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+                </nav>
+            </ul>
+
+        </div>
     </div>
-  </div>
 </div>
