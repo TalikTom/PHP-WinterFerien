039fed4b89e479e3dbd732ef84fb3ecddb577baa
Luka Agic <lukagic@gmail.com>
2023-01-01 23:15:27 +0100

    Style nav, bootstrap

--- bootstrap/views/partials/nav.php
+++ bootstrap/views/partials/nav.php
@@ -1,4 +1,4 @@
-<header class="p-3 mb-3 border-bottom">
+<header class="p-3 bg-dark text-light">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
@@ -9,15 +9,15 @@
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="/bootstrap/"
-                        class="nav-link px-2 link-<?=urlIs('/bootstrap/') ? 'secondary' : 'dark'?>">Home</a></li>
+                        class="nav-link px-2 link-<?=urlIs('/bootstrap/') ? 'primary' : 'light'?>">Home</a></li>
                 <li><a href="/bootstrap/about"
-                        class="nav-link px-2 link-<?=urlIs('/bootstrap/about') ? 'secondary' : 'dark'?>">About
+                        class="nav-link px-2 link-<?=urlIs('/bootstrap/about') ? 'primary' : 'light'?>">About
                         us</a></li>
                 <li><a href="/bootstrap/calendar"
-                        class="nav-link px-2 link-<?=urlIs('/bootstrap/calendar') ? 'secondary' : 'dark'?>">Calendar</a>
+                        class="nav-link px-2 link-<?=urlIs('/bootstrap/calendar') ? 'primary' : 'light'?>">Calendar</a>
                 </li>
                 <li><a href="/bootstrap/contact"
-                        class="nav-link px-2 link-<?=urlIs('/bootstrap/contact') ? 'secondary' : 'dark'?>">Contact
+                        class="nav-link px-2 link-<?=urlIs('/bootstrap/contact') ? 'primary' : 'light'?>">Contact
                         us</a></li>
 
             </ul>
