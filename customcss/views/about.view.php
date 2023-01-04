8970829edf4b5d811a5cefb844efe94d5e780db2
Luka Agic <lukagic@gmail.com>
2023-01-04 13:47:56 +0100

    Add content and style about page, customcss

--- customcss/views/about.view.php
+++ customcss/views/about.view.php
@@ -1,4 +1,48 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/nav.php'; ?>
 <?php include 'partials/banner.php'; ?>
+<div class="about-wrapper">
+    <h1 class="about-heading">About us</h1>
+    <section class="about-section">
+
+        <article>
+            <h2>First article</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae, laborum velit atque porro
+                voluptatum officia suscipit harum, reprehenderit dolorem et, possimus deleniti eos commodi maiores
+                ducimus exercitationem inventore? Corporis quasi quisquam quae necessitatibus suscipit hic, repudiandae
+                dolor tempora aperiam distinctio nisi asperiores illum quidem? Ut nihil quibusdam hic. Dolores quos
+                facere soluta omnis! Hic earum harum accusantium officia. Nisi ratione ea, incidunt praesentium
+                laboriosam ex natus nemo similique molestiae facilis nobis fugiat. Recusandae voluptatibus ex totam
+                soluta aliquid. Itaque?</p>
+        </article>
+
+        <article>
+            <h2>Second article</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae, laborum velit atque porro
+                voluptatum officia suscipit harum, reprehenderit dolorem et, possimus deleniti eos commodi maiores
+                ducimus exercitationem inventore? <em>Corporis</em> quasi quisquam quae necessitatibus suscipit hic,
+                repudiandae
+                dolor tempora aperiam distinctio nisi asperiores illum quidem? Ut nihil quibusdam hic. Dolores quos
+                facere soluta omnis! Hic earum harum accusantium officia. Nisi ratione ea, incidunt praesentium
+                laboriosam ex natus nemo similique molestiae facilis nobis fugiat. Recusandae voluptatibus ex totam
+                soluta aliquid. Itaque?</p>
+        </article>
+
+        <article>
+            <h2>Third article</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae, laborum velit atque porro
+                voluptatum officia suscipit harum, reprehenderit dolorem et, possimus deleniti eos commodi maiores
+                ducimus exercitationem inventore? Corporis quasi quisquam quae necessitatibus suscipit hic, repudiandae
+                dolor tempora aperiam distinctio nisi asperiores illum quidem? Ut nihil quibusdam hic. Dolores quos
+                facere soluta omnis! Hic earum harum accusantium officia. Nisi ratione ea, incidunt praesentium
+                laboriosam ex natus nemo similique molestiae facilis nobis fugiat. Recusandae voluptatibus ex totam
+                soluta aliquid. Itaque?</p>
+
+
+        </article>
+    </section>
+</div>
 <?php include 'partials/footer.php'; ?>
