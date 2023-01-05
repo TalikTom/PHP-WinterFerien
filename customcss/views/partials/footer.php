a93fe5e0aa5f5b1cc8b2ec5f14de4c31b17426a9
Luka Agic <lukagic@gmail.com>
2023-01-05 22:13:08 +0100

    Add footer content and style, customcss

--- customcss/views/partials/footer.php
+++ customcss/views/partials/footer.php
@@ -1,6 +1,33 @@
 </div>
 <footer>
-    &copy;
+    <div class="footer_wrapper">
+        <div class="link_container">
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+        <div class="link_container">
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+        <div class="link_container">
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+    </div>
+
+    © <?= $year ?> Copyright: LA
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 <script src="/customcss/js/script.js"></script>
--- foundation/views/contact.view.php
+++ foundation/views/contact.view.php
@@ -1,7 +1,40 @@
 <?php require 'partials/header.php'; ?>
 <?php require 'partials/nav.php'; ?>
-<?php require 'partials/banner.php'; ?>
 
 
+
+<form>
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x">
+            <div class="medium-6 cell">
+                <label>Name
+                    <input type="text" placeholder=".medium-6.cell">
+                </label>
+            </div>
+            <div class="medium-6 cell">
+                <label>Email
+                    <input type="text" placeholder=".medium-6.cell">
+                </label>
+            </div>
+        </div>
+        <div class="grid-x grid-padding-x">
+
+            <div class="medium-6 cell">
+                <label>Subject
+                    <input type="text" placeholder=".medium-6.cell">
+                </label>
+            </div>
+        </div>
+        <div class="grid-x grid-padding-x">
+
+            <div class="medium-6 cell">
+                <label>
+                    What books did you read over summer break?
+                    <textarea placeholder="None"></textarea>
+                </label>
+            </div>
+        </div>
+    </div>
+</form>
 <?php require 'partials/footer.php'; ?>
 
