7115605cad4522d5d3aea90d0e8066eb02ff9594
Luka Agic <agic.luke@gmail.com>
2023-01-07 23:40:00 +0100

    Add footer, foundation

--- foundation/functions.php
+++ foundation/functions.php
@@ -3,3 +3,5 @@
 function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
 }
+
+$year = date('Y');
--- foundation/views/partials/footer.php
+++ foundation/views/partials/footer.php
@@ -1,6 +1,26 @@
+<footer>
+    <div class="grid-x align-center">
+        <ul class="cell large-5">
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+        </ul>
+        <ul class="cell large-5">
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+        </ul>
+    </div>
+    &copy <?= $year ?> Copyright: LA
+</footer>
 <script src="/foundation/js/vendor/jquery.js"></script>
-<script src="/foundation/js/vendor/foundation.js"></script>
 <script src="/foundation/js/vendor/what-input.js"></script>
+<script src="/foundation/js/vendor/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
 <script src="/foundation/js/app.js"></script>
 </body>
 </html>
