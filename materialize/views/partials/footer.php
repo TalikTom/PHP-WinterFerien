484bcb63c9160910428609a22fb70cc5cc2c3434
Luka Agic <agic.luke@gmail.com>
2023-01-08 00:11:02 +0100

    Add footer, materialize

--- materialize/views/partials/footer.php
+++ materialize/views/partials/footer.php
@@ -1,3 +1,33 @@
+<footer class="page-footer grey darken-4">
+    <div class="container">
+        <div class="row">
+
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright center-align">
+        <div class="container">
+            © 2014 Copyright Text
+        </div>
+    </div>
+</footer>
 </body>
 
 </html>
