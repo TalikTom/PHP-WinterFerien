f12168d0a286e0c754b03c476c25d94d03780684
Luka Agic <agic.luke@gmail.com>
2023-01-09 13:33:18 +0100

    Add responsive images to banner, foundation

--- foundation/views/partials/banner.php
+++ foundation/views/partials/banner.php
@@ -5,13 +5,25 @@
                 <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
                 <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
                 <li class="is-active orbit-slide">
-                    <img class="orbit-image" src="/foundation/img/image1.jpg" alt="Space">
+                    <picture>
+                        <source media="(max-width:650px)" srcset="/foundation/img/image1-650.jpg">
+                        <source media="(max-width:450px)" srcset="/foundation/img/image1-450.jpg">
+                        <img src="/foundation/img/image1.jpg" alt="Elder care">
+                    </picture>
                 </li>
                 <li class="orbit-slide">
-                    <img class="orbit-image" src="/foundation/img/image2.jpg" alt="Space">
+                    <picture>
+                        <source media="(max-width:650px)" srcset="/foundation/img/image2-650.jpg">
+                        <source media="(max-width:450px)" srcset="/foundation/img/image2-450.jpg">
+                        <img src="/foundation/img/image3.jpg" alt="Elder care">
+                    </picture>
                 </li>
                 <li class="orbit-slide">
-                    <img class="orbit-image" src="/foundation/img/image3.jpg" alt="Space">
+                    <picture>
+                        <source media="(max-width:650px)" srcset="/foundation/img/image3-650.jpg">
+                        <source media="(max-width:450px)" srcset="/foundation/img/image3-450.jpg">
+                        <img src="/foundation/img/image3.jpg" alt="Elder care">
+                    </picture>
                 </li>
                 <nav class="orbit-bullets">
                     <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span
