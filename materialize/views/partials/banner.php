3c2ced040d259d69e0e712189d18322f3f5b6929
Luka Agic <agic.luke@gmail.com>
2023-01-09 13:37:35 +0100

    Add responsive images to banner, materialize

--- materialize/views/partials/banner.php
+++ materialize/views/partials/banner.php
@@ -1,13 +1,31 @@
 <script>
-$(document).ready(function() {
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true
+    $(document).ready(function () {
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            indicators: true
+        });
     });
-});
 </script>
 <div class="carousel carousel-slider">
-    <a class="carousel-item" href="#one!"><img src="/materialize/img/image1.jpg"></a>
-    <a class="carousel-item" href="#two!"><img src="/materialize/img/image2.jpg"></a>
-    <a class="carousel-item" href="#three!"><img src="/materialize/img/image3.jpg"></a>
+    <a class="carousel-item" href="#one!">
+        <picture>
+            <source media="(max-width:650px)" srcset="/materialize/img/image1-650.jpg">
+            <source media="(max-width:450px)" srcset="/materialize/img/image1-450.jpg">
+            <img src="/materialize/img/image1.jpg" alt="Elder care">
+        </picture>
+    </a>
+    <a class="carousel-item" href="#two!">
+        <picture>
+            <source media="(max-width:650px)" srcset="/materialize/img/image2-650.jpg">
+            <source media="(max-width:450px)" srcset="/materialize/img/image2-450.jpg">
+            <img src="/materialize/img/image2.jpg" alt="Elder care">
+        </picture>
+    </a>
+    <a class="carousel-item" href="#three!">
+        <picture>
+            <source media="(max-width:650px)" srcset="/materialize/img/image3-650.jpg">
+            <source media="(max-width:450px)" srcset="/materialize/img/image3-450.jpg">
+            <img src="/materialize/img/image3.jpg" alt="Elder care">
+        </picture>
+    </a>
 </div>
