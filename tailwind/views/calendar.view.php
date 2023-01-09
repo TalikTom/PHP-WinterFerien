5a6797733080b88297e4bbc118c7c790d8f9b7a1
Luka Agic <agic.luke@gmail.com>
2023-01-09 17:01:47 +0100

    Update calendar.view.php

--- tailwind/views/calendar.view.php
+++ tailwind/views/calendar.view.php
@@ -1,13 +1,7 @@
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/nav.php'; ?>
-<?php require 'partials/banner.php'; ?>
 
-<main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <p>This is a page for a calendar</p>
-        <!-- /End replace -->
-    </div>
-</main>
+Here goes calendar
+
 
 <?php require 'partials/footer.php'; ?>
