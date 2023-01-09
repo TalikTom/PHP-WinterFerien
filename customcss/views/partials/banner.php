8fcb5ab08823884a7fce124eb03b514e1b4ffde8
Luka Agic <agic.luke@gmail.com>
2023-01-09 12:56:14 +0100

    Add responsive images to banner, customcss

--- customcss/views/partials/banner.php
+++ customcss/views/partials/banner.php
@@ -8,8 +8,21 @@
                 <img src="/customcss/img/image1.jpg" alt="Elder care">
             </picture>
         </div>
-        <div class="swiper-slide"><img src="/customcss/img/image2.jpg" alt="Elder care"></div>
-        <div class="swiper-slide"><img src="/customcss/img/image3.jpg" alt="Elder care"></div>
+        <div class="swiper-slide">
+            <picture>
+                <source media="(max-width:650px)" srcset="/customcss/img/image2-650.jpg">
+                <source media="(max-width:450px)" srcset="/customcss/img/image2-450.jpg">
+                <img src="/customcss/img/image2.jpg" alt="Elder care">
+            </picture>
+        </div>
+
+        <div class="swiper-slide">
+            <picture>
+                <source media="(max-width:650px)" srcset="/customcss/img/image2-650.jpg">
+                <source media="(max-width:450px)" srcset="/customcss/img/image2-450.jpg">
+                <img src="/customcss/img/image3.jpg" alt="Elder care">
+            </picture>
+        </div>
     </div>
     <div class="swiper-pagination"></div>
 </div>
