1ba37029b3ed76ea1ac76467b42175eae612e12f
Luka Agic <agic.luke@gmail.com>
2023-02-17 22:46:37 +0100

    Create session.php

---
+++ customcss/session.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+$login = $_SESSION['login'] ?? false;
+
+$email = 'test@gmail.com';
+$passwod = 'root';
+
+function login()
+{
+    session_regenerate_id(true);
+    $_SESSION['login'] = true;
+
+}
+
+function logout()
+{
+    $_SESSION = [];
+
+    $params = session_get_cookie_params();
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+
+    session_destroy();
+}
+
+function require_login($login)
+{
+    if ($login == false) {
+        header('Location: login.php');
+        exit;
+    }
+}
